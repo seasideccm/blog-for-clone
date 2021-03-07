3db38c6903d45402a1b0ac521f515572e66d8097
seasideccm <2094558239@qq.com>
2021-03-07 16:40:08 +0800

    deploy md first

---
+++ docs/.vuepress/config/themeConfig/nav.js
@@ -0,0 +1,38 @@
+// nav
+module.exports = [
+  { text: '首页', link: '/' },
+  {
+    text: '血流理论',
+    link: '/bloodflow/theory/', 
+  },
+  {
+    text: '血流技术',
+    link: '/bloodflow/technology/',
+  },
+  
+  {
+    text: '临床血流',
+    link: '/bloodflow/clinic/',   
+
+  },  
+  
+  
+   
+  { 
+    text: '关于', link: '/pages/ad735e/' 
+  },
+  
+  
+  
+  
+  {
+    text: '索引',
+    link: '/archives/',
+    items: [
+      { text: '分类', link: '/categories/' },
+      { text: '标签', link: '/tags/' },
+      { text: '归档', link: '/archives/' },
+    ], 
+  },
+
+]
